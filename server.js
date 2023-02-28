3e9df5d03312efa90f586d31992d5c5af34530bb
Franco Morroni <fmorroni99@gmail.com>
2023-02-28 00:39:42 -0300

    Add express server
    Will be used to receive the curriculums from the scraping script and storing it in a database with sequelize

---
+++ server.js
@@ -0,0 +1,27 @@
+// Run with node server.js and ngrok http --host-header=rewrite 5000
+
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.disable('etag');
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res, next) => {
+  res.json({msg: 'Working'})
+})
+
+// app.options('*', cors())
+// app.options('/curriculum', (req, res) => {
+//   res.status(200).end()
+// })
+app.post('/curriculum', (req, res) => {
+  console.log('Post request body: ', req.body)
+  res.json({status: 200})
+})
+
+app.listen(5000, () => {
+  console.log('CORS-enabled web server listening on port 5000')
+})
