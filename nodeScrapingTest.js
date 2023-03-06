e19ebcc00bfba71e0c8236621844b8bc50d78f1c
Franco Morroni <fmorroni99@gmail.com>
2023-03-05 23:20:10 -0300

    Updates
    Still not working from outside sga domain

--- nodeScrapingTest.js
+++ nodeScrapingTest.js
@@ -4,9 +4,19 @@ import sga_credentials from './.sga_credentials.js'
 const baseUrl = 'https://sga.itba.edu.ar/app2/'
 async function login() {
   try {
-    const loginPageRes = await fetch(baseUrl)
-    
+    const loginPageRes = await fetch(baseUrl, {omit: true})
+
     const loginPageHtml = await loginPageRes.text()
+    const doc = new DOMParser().parseFromString(loginPageHtml, 'text/html')
+    const base = document.createElement('base')
+    base.href = baseUrl + 'asdf/asdf/asdf/'
+    doc.head.insertBefore(base, doc.head.children[0])
+
+    const root = document.querySelector('html')
+    root.replaceChildren()
+    root.appendChild(doc.head)
+    root.appendChild(doc.body)
+
     const path = loginPageHtml.match(/action="(?:\.\.\/)*(.*?)"/)[1]
     const id = loginPageHtml.match(/input.*?id="(id[0-9a-zA-Z]+_[0-9a-zA-Z]{2,}_[0-9a-zA-Z]{1,})"/)[1]
 
@@ -27,12 +37,23 @@ async function login() {
     const postCredentialsHtml = await postCredentialsRes.text()
     console.log(postCredentialsHtml)
 
-    return new JSDOM(postCredentialsHtml).window.document
+    return new DOMParser().parseFromString(postCredentialsHtml, 'text/html')
   } catch (err) {
     console.log(err)
   }
 }
 
-login()
-.then(doc => console.log(doc.body))
-.catch(err => console.log(err))
+async function prepSite() {
+  const doc = await login()
+  const base = document.createElement('base')
+  base.href = baseUrl
+  doc.head.insertBefore(base, doc.head.children[0])
+  
+  const root = document.querySelector('html')
+  root.replaceChildren()
+  root.appendChild(doc.head)
+  root.appendChild(doc.body)
+  console.log('asdf')
+}
+
+prepSite()
